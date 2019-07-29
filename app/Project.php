111152bff9eaf7a6d48d7b0bd70a8e4dbfd9e3e8
Mohamed <drakelogan52@gmail.com>
2019-07-29 14:50:55 +0200

    created project model and migration and set up reationship between project and user

---
+++ app/Project.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model {
+
+    // returns the user who created the project
+    public function User() {
+        return $this->belongsTo(User::class);
+    }
+}
